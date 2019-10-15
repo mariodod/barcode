b8b308b59f0bcbe0db583cf2ce0328959bab9ef6
Mario Del Olmo Diaz <mario.del.olmo.diaz@cern.ch>
2019-10-15 16:41:58 +0200

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,11 @@ module.exports = {
         extensions: ['.js', '.jsx']
     },
     output: {
-        path: __dirname + '/dist',
+        path: __dirname + '/public',
         publicPath: '/',
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './dist'
+        contentBase: './public'
     }
 };
